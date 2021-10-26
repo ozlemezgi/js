0a98a365e42ecd7b57e1f1835006ba55d348a6f7
ozlemezgi <ozlemezgisari@icloud.com>
2021-10-26 14:57:21 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+console.log('merhaba')
+
+let dolarDun = 9.20
+
+let dolarBugun = 9.30
+
+dolarDun='9.20'
+{
+    let dolarDun = 9.10
+}
+
+ console.log(dolarDun)
+
+ const euroDun =11.2
+ //euroDun =11 ----->HATA
+
+ console.log(euroDun)
+//array
+ let konutKredileri = ['konut kredisi','emlak konut kredisi','kamu konut kredisi','özel konut kredileri']
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+ 
+
+console.log(konutKredileri)
